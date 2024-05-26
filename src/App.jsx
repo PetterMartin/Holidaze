ea1607e41f16c743e3a0746c47593ecc7edda7f0
PetterMartin <petter.aa@icloud.com>
2024-05-26 18:23:44 +0200

    added footer to home

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { Outlet } from "@tanstack/react-router";
 import Navbar from "./components/nav/Navbar";
-import Footer from "./components/nav/Footer";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Outlet />
-      <Footer />
     </>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import SearchBar from "../components/nav/Searchbar";
 import AllVenues from "../components/profile/AllVenues";
 import useVenueSearch from "../hooks/useVenueSearch";
 import Highlights from "../components/carousel/Highlights";
+import Footer from "../components/nav/Footer";
 
 const Home = () => {
   const { venues, searchVenues } = useVenueSearch();
@@ -34,10 +35,11 @@ const Home = () => {
         setGuestNumber(guests);
         setSearchClicked(true);
         await searchVenues({ guests, searchText }); // Pass selectedDates here
-  
+
         // Scroll to AllVenues section with an offset
         if (allVenuesRef.current) {
-          const elementPosition = allVenuesRef.current.getBoundingClientRect().top;
+          const elementPosition =
+            allVenuesRef.current.getBoundingClientRect().top;
           const offsetPosition = elementPosition + window.pageYOffset;
           window.scrollTo({
             top: offsetPosition,
@@ -57,41 +59,46 @@ const Home = () => {
   };
 
   return (
-    <div className="flex flex-col items-center relative">
-      <div className="relative w-full h-[450px]">
-        <img
-          src="/assets/images/House.jpeg"
-          alt="Background"
-          className="w-full h-full object-cover"
-        />
-        <div className="absolute inset-0 bg-gray-700 opacity-30"></div>
+    <>
+      <div className="flex flex-col items-center relative">
+        <div className="relative w-full h-[450px]">
+          <img
+            src="/assets/images/House.jpeg"
+            alt="Background"
+            className="w-full h-full object-cover"
+          />
+          <div className="absolute inset-0 bg-gray-700 opacity-30"></div>
+        </div>
+        <div className="text-container flex flex-col items-center gap-2 absolute top-36 left-[50%] transform translate-x-[-50%] -translate-y-[-50%] text-white z-10">
+          <h1 className="with-shadow text-6xl font-bold">
+            Apartments for a day
+          </h1>
+          <p className="text-lg">Find and choose the perfect home for you</p>
+        </div>
+        <div className="absolute top-[390px] w-full z-10">
+          <SearchBar
+            onSearch={handleSearch}
+            selectedLayout={selectedLayout}
+            handleLayoutClick={handleLayoutClick}
+          />
+        </div>
+        <Highlights onSearch={handleSearch} />
+        <div className="mt-auto" ref={allVenuesRef}>
+          <AllVenues
+            venues={venues}
+            selectedVenueId={selectedVenueId}
+            setSelectedVenueId={setSelectedVenueId}
+            isModalOpen={isModalOpen}
+            setIsModalOpen={setIsModalOpen}
+            searchText={searchText}
+            guests={guestNumber}
+            searchClicked={searchClicked}
+            selectedLayout={selectedLayout}
+          />
+        </div>
       </div>
-      <div className="text-container flex flex-col items-center gap-2 absolute top-36 left-[50%] transform translate-x-[-50%] -translate-y-[-50%] text-white z-10">
-        <h1 className="with-shadow text-6xl font-bold">Apartments for a day</h1>
-        <p className="text-lg">Find and choose the perfect home for you</p>
-      </div>
-      <div className="absolute top-[390px] w-full z-10">
-      <SearchBar
-          onSearch={handleSearch}
-          selectedLayout={selectedLayout}
-          handleLayoutClick={handleLayoutClick}
-        />
-      </div>
-      <Highlights onSearch={handleSearch}/>
-      <div className="mt-auto" ref={allVenuesRef}>
-        <AllVenues
-          venues={venues}
-          selectedVenueId={selectedVenueId}
-          setSelectedVenueId={setSelectedVenueId}
-          isModalOpen={isModalOpen}
-          setIsModalOpen={setIsModalOpen}
-          searchText={searchText}
-          guests={guestNumber}
-          searchClicked={searchClicked}
-          selectedLayout={selectedLayout}
-        />
-      </div>
-    </div>
+      <Footer />
+    </>
   );
 };
 
